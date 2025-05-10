efb7e30e048e93fb41f8dad9fc5013a3f71e4431
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2025-05-10 11:53:01 +0300

    change user in main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "libvirt" {
-  uri = "qemu+ssh://m_amin:admin@127.0.0.1:22/system?sshauth=ssh-password"
+  uri = "qemu+ssh://eng_muhammedsabry:admin@127.0.0.1:22/system?sshauth=ssh-password"
 }
 
 
