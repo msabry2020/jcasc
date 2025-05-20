9e6fb27d65faab8ce94027c0104ef18b2181f762
msabry2020 <69658996+msabry2020@users.noreply.github.com>
2025-05-20 17:08:02 +0300

    seed2 ok

--- seed2.groovy
+++ seed2.groovy
@@ -1,11 +1,17 @@
-pipelineJob('TF Apply') {
+pipelineJob('TF_new_Apply') {
   parameters {
     stringParam 'VM_NAME', '', 'VM Name to provision'
   }
   definition {
       cpsScm {
           scm {
-              git('https://github.com/msabry2020/jcasc.git','main')
+              git{
+                remote{
+                  url('https://github.com/msabry2020/jcasc.git')
+                }
+                branch('main')
+                extensions { }
+              }
           }
           scriptPath('tf_apply.groovy')
       }
